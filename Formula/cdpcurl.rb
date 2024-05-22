a4aa1dee5f31576cddd68076bafa8585b34cc2f4
Di Mei <di.mei@coinbase.com>
2024-05-22 11:43:24 -0400

    Update cdpcurl.rb

--- Formula/cdpcurl.rb
+++ Formula/cdpcurl.rb
@@ -2,7 +2,7 @@ class Cdpcurl < Formula
   desc "curl with CDP API key"
   homepage "https://github.com/coinbase/cdpcurl"
   url "https://github.com/coinbase/cdpcurl/archive/refs/tags/v0.0.1.tar.gz"
-  sha256 "05cf14fe02739e70598625ecec39bd5912090e055993ba4514609e91cb54550a"
+  sha256 "8a0f88d932a5d329f513733b812dcb46400db5503ad5c99d6663e52ae358047b"
   license "MIT"
 
   depends_on "go" => :build
